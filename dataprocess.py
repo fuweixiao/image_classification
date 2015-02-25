3da4e052d33f6bc55d13854de881d1c87077639e
fuweixiao <weixiaofu2014@u.northwestern.edu>
2015-02-24 23:09:15 -0500

    update dataprocess

--- dataprocess.py
+++ dataprocess.py
@@ -1,16 +1,44 @@
 import json
 import urllib
-from pprint import pprint
-json_data=open('pin.json')
+import os
 
+#set up input and output
+inputpath =  'orig.json'
+outputpath = '/home/smile/pinterest/'
+try:
+    os.mkdir(outputpath)
+except:
+    pass
+
+
+
+#deal with useless lines which cannot be parsed
+lines = open(inputpath).readlines()
+for i in range(0, len(lines)):
+    if lines[i].find('ObjectId') != -1:
+        del lines[i]
+        i = i - 1
+    if lines[i].find('description') != -1 and lines[i].find('html') == -1:
+        del lines[i]
+        break
+open('pin.json', 'w').writelines(lines)
+
+#parse json
+json_data=open('pin.json')
 data = json.load(json_data)
+
+#download and save images
 url = data["images"]["236x"]["url"]
 strs = url.split(' ')
 url = ""
 for str in strs:
     url = url + str 
 file_name = url.split('/')[-1]
-urllib.urlretrieve (url, file_name)
-print data["board"]["category"]
+category = data["board"]["category"]
+try:
+    os.mkdir(outputpath + category)
+except:
+    pass
+urllib.urlretrieve (url, outputpath + category + '/' + file_name)
 json_data.close()
 
