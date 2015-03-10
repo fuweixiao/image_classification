4c7c7795b3ca822beec9b83f540188700aeac0b5
fuweixiao <weixiaofu2014@u.northwestern.edu>
2015-03-09 22:44:36 -0400

    deal with new data

--- dataprocess.py
+++ dataprocess.py
@@ -3,7 +3,7 @@ import urllib
 import os
 
 #set up input and output
-inputpath =  '/home/smile/workspace/image_classification/orig.json'
+inputpath =  '/home/smile/workspace/pinterest/data/pin_pins.json'
 outputpath = '/home/smile/pinterest/'
 dictfile = '/home/smile/workspace/image_classification/result-final.txt'
 try:
@@ -21,44 +21,48 @@ for line in lines:
     dictionary[key] = kvpair
 
 #deal with useless lines which cannot be parsed
-lines = open(inputpath).readlines()
-for i in range(0, len(lines)):
-    if lines[i].find('ObjectId') != -1:
-        del lines[i]
-        i = i - 1
-    if lines[i].find('description') != -1 and lines[i].find('html') == -1:
-        del lines[i]
-        break
-open('pin.json', 'w').writelines(lines)
+#lines = open(inputpath).readlines()
+#for i in range(0, len(lines)):
+#    if lines[i].find('ObjectId') != -1:
+#        del lines[i]
+#        i = i - 1
+#    if lines[i].find('description') != -1 and lines[i].find('html') == -1:
+#        del lines[i]
+#        break
+#open('pin.json', 'w').writelines(lines)
 
 #parse json
-json_data=open('pin.json')
-data = json.load(json_data)
-
-#download and save images
-url = data["images"]["236x"]["url"]
-strs = url.split(' ')
-url = ""
-for str in strs:
-    url = url + str 
-file_name = url.split('/')[-1]
-
-category = data["board"]["category"]
-dirname = category
-if dictionary.has_key(category):
-    for value in dictionary[category]:
-        dirname = value.split('\n')[0]
+json_file = open(inputpath)
+json_data = json_file.readline(); 
+while json_data:
+    data = json.loads(json_data)
+    #download and save images
+    url = data["images"]["236x"]["url"]
+    #strs = url.split(' ')
+    #url = ""
+    #for str in strs:
+    #    url = url + str 
+    file_name = url.split('/')[-1]
+    
+    category = data["board"]["category"]
+    if category == None:
+        category = "unknown"
+    dirname = category
+    if dictionary.has_key(category):
+        for value in dictionary[category]:
+            dirname = value.split('\n')[0]
+            try:
+                os.mkdir(outputpath + dirname)
+            except:
+                pass
+            urllib.urlretrieve (url, outputpath + dirname + '/' + file_name)
+    else:
         try:
             os.mkdir(outputpath + dirname)
         except:
             pass
         urllib.urlretrieve (url, outputpath + dirname + '/' + file_name)
-else:
-    try:
-        os.mkdir(outputpath + dirname)
-    except:
-        pass
-    urllib.urlretrieve (url, outputpath + dirname + '/' + file_name)
-
-json_data.close()
+    json_data = json_file.readline() 
+    
+json_file.close()
 
